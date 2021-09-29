1046217c750e2437eedd1a761142eff4fa441219
AdriFC <adri86vigo@gmail.com>
2021-09-29 12:41:10 +0200

    Agregación más hojas de estilo

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -10,9 +10,13 @@ add_action('init', 'gymfitness_menus');
 
 //Scripts y Styles
 function gymfitness_scripts_styles() {
+    //Normalize
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+    //Google fonts
+    wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
     //La hoja de estilos principal
-    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
-}
+    wp_enqueue_style('style', get_stylesheet_uri('normalize', 'googleFont'), array(), '1.0.0');
+}    
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
 
 ?>
