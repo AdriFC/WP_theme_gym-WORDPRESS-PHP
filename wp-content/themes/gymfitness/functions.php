ac026f7b1111bc6fa7b40c5a02c1eaff8f86c287
AdriFC <adri86vigo@gmail.com>
2021-09-30 11:00:00 +0200

    Creación de footer, página sobre nosotros y template sin sidebar.

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -1,5 +1,20 @@
 <?php
 
+//Cuando el tema es activado
+function gymfitness_setup() {
+
+    //Habilitar imagenes destacadas
+    add_theme_support('post-thumbnails');
+
+    //Agregar imágenes de tamaño personalizado
+    add_image_size('square', 350, 350, true);
+    add_image_size('portrait', 350, 724, true);
+    add_image_size('cajas', 400, 375, true);
+    add_image_size('mediano', 700, 400, true);
+    add_image_size('blog', 966, 644, true);
+}
+add_action('after_setup_theme', 'gymfitness_setup');
+
 //Menús de navegación, agregar más utilizando el arreglo
 function gymfitness_menus() {
     register_nav_menus(array(
--- wp-content/themes/gymfitness/page-no-sidebars.php
+++ wp-content/themes/gymfitness/page-no-sidebars.php
@@ -1,10 +1,17 @@
+<?php 
+
+/*
+Template Name: Contenido centrado (No Sidebars)
+ */
+
+?>
+
 <?php get_header(); ?>
 
-<main class="contenedor pagina seccion con-sidebar">
+<main class="contenedor pagina seccion no-sidebar">
     <div class="contenido-principal">
         <?php get_template_part('template-parts/paginas'); ?>
     </div>
-    <?php get_sidebar(); ?>
 </main>
 
 <?php get_footer(); ?>
