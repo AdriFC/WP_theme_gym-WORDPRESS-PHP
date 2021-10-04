ac4665f1062543196ebbeb558b148c2ec31c83eb
AdriFC <adri86vigo@gmail.com>
2021-10-04 09:29:08 +0200

    Creación de la galería con Lightbox.

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -28,16 +28,32 @@ add_action('init', 'gymfitness_menus');
 
 //Scripts y Styles
 function gymfitness_scripts_styles() {
+
     //Normalize
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
     //SlickNav CSS
     wp_enqueue_style('slicknavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.0');
+
+    //LightBox CSS
+    if(is_page('galeria')):
+        wp_enqueue_style('lightboxCSS', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.3');
+    endif;
+
     //Google fonts
     wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
+
     //La hoja de estilos principal
     wp_enqueue_style('style', get_stylesheet_uri('normalize', 'googleFont'), array(), '1.0.0');
+
     //SlickNav JS
     wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.0', true);
+
+    //LightBox JS
+    if(is_page('galeria')):
+        wp_enqueue_script('lightboxJS', get_template_directory_uri() . '/js/jquery.lightbox.min.js', array('jquery'), '2.11.3', true);
+    endif;
+    
     //Scripts
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slicknavJS'), '1.0.0', true);
 }    
