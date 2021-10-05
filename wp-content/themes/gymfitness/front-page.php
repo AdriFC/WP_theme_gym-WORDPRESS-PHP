1519caaa03d1029537f59791d7668f6a1c08bb98
AdriFC <adri86vigo@gmail.com>
2021-10-05 13:03:10 +0200

    Finalizado template de página principal

--- wp-content/themes/gymfitness/front-page.php
+++ wp-content/themes/gymfitness/front-page.php
@@ -1,4 +1,60 @@
 <?php get_header( 'front' ); ?>
 
+<section class="bienvenida text-center seccion">
+    <h2 class="texto-primario"><?php the_field('encabezado_bienvenida'); ?></h2>
+    <p><?php the_field('texto_bienvenida') ?></p>
+</section>
+
+<div class="seccion-areas">
+    <ul class="contenedor-areas">
+        <li class="area">
+            <?php
+                $area1 = get_field('area_1');
+                $imagen = wp_get_attachment_image_src($area1['area_imagen'], 'mediano')[0];
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>"/>
+            <p><?php echo esc_html ($area1['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php
+                $area2 = get_field('area_2');
+                $imagen = wp_get_attachment_image_src($area2['area_imagen'], 'mediano')[0];
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>"/>
+            <p><?php echo esc_html ($area2['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php
+                $area3 = get_field('area_3');
+                $imagen = wp_get_attachment_image_src($area3['area_imagen'], 'mediano')[0];
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>"/>
+            <p><?php echo esc_html ($area3['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php
+                $area4 = get_field('area_4');
+                $imagen = wp_get_attachment_image_src($area4['area_imagen'], 'mediano')[0];
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>"/>
+            <p><?php echo esc_html ($area4['area_texto']); ?></p>
+        </li>
+    </ul>
+</div>
+
+<section class="clases">
+    <div class="contenedor seccion">
+        <h2 class="text-center texto-primario">Nuestras clases</h2>
+        <?php gymfitness_lista_clases(4); ?>
+
+        <div class="contenedor-boton">
+            <a href="<?php echo esc_url( get_permalink( get_page_by_title('Nuestras clases'))); ?>"
+            class="boton boton-primario"> Ver todas las clases</a>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
